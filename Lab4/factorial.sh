82747aa5bc00fcd866201871e507d1d87b15edbd
Pranav-jagadeesh <53830306+Pranav-jagadeesh@users.noreply.github.com>
2020-11-05 16:21:36 +0530

    PRANAV_JAGADEESH_1BM18CS071_5B_Batch-2
    PRANAV_JAGADEESH_1BM18CS071_5B_Batch-2

---
+++ Lab4/factorial.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+echo "Enter a number"
+read num
+n=$num
+
+fact=1
+
+while [ $num -gt 1 ]
+
+do
+
+  fact=`expr  $fact \* $num`
+
+  num=`expr $num - 1`
+
+done
+
+echo "Factorial of $n is $fact"
